60fd4cac44db3db5f987bdb8f0f6b577a6bf0d0e
leo-capvano <60476114+leo-capvano@users.noreply.github.com>
2024-08-13 16:14:26 +0200

    develop: added chatbot skeleton

---
+++ app/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+from llm_svc import generate_response
+
+st.title("LazyGPT")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# load all prompt templates here
+st.sidebar.write("ciao")
+
+if user_input := st.chat_input("Input a message"):
+    st.chat_message("human").markdown(user_input)
+    st.session_state.messages.append({"role": "human", "content": user_input})
+
+    # process input with llm
+    assistant_output_stream = generate_response(user_input=user_input)
+    st.chat_message("assistant").write_stream(assistant_output_stream)
+    st.session_state.messages.append({"role": "assistant", "content": assistant_output_stream})
---
+++ app/llm_svc.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+base_prompt_template = "You are a helpful assistant that responds to user's input."
+translator_prompt_template = "You are a helpful assistant that translates every user's input from italian to english."
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", translator_prompt_template),
+        ("human", "{user_input}"),
+    ]
+)
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+
+def generate_response(user_input: str) -> str:
+    for chunk in  (prompt | llm).stream({"user_input": user_input}):
+        yield chunk
