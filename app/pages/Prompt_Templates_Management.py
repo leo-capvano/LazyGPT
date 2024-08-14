7c6708c2661608d0efd2cca581db5c6f9499c707
leo-capvano <60476114+leo-capvano@users.noreply.github.com>
2024-08-14 19:06:55 +0200

    prompt template management: - delete pt - copy pt

--- app/pages/Prompt_Templates_Management.py
+++ app/pages/Prompt_Templates_Management.py
@@ -7,6 +7,12 @@ sys.path.append(os.path.join("..", "service"))
 
 from service.prompt_template_svc import get_all_prompt_templates
 from service.prompt_template_svc import save_prompt_template
+from service.prompt_template_svc import delete_prompt_template
+
+
+def delete_pt(pt_file_name: str):
+    delete_prompt_template(pt_file_name)
+
 
 st.title("Prompt Templates Management")
 
@@ -18,6 +24,9 @@ if st.button("Save") and pt_name and pt_text:
 
 st.divider()
 st.markdown("## Prompt Templates Catalog")
-prompt_templates = get_all_prompt_templates()
+prompt_templates: list[dict[str, str]] = get_all_prompt_templates()
 for pt in prompt_templates:
-    st.write(pt)
+    with st.container(border=True):
+        st.write(f"### Prompt template name: {pt.get("pt_name").split(".")[0]}")
+        st.code(pt.get("pt_text"))
+        st.button("Delete", use_container_width=True, on_click=delete_pt, args=(pt.get("pt_name"),))
--- app/service/prompt_template_svc.py
+++ app/service/prompt_template_svc.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Dict
 
 TEMPLATES_FOLDER = "templates"
 
@@ -9,11 +9,16 @@ def save_prompt_template(template_name: str, template_text: str):
         file.write(template_text)
 
 
-def get_all_prompt_templates() -> List[str]:
-    templates: List[str] = []
+def get_all_prompt_templates() -> list[dict[str, str]]:
+    templates: List[Dict[str, str]] = list()
     entries = os.listdir(TEMPLATES_FOLDER)
     template_files = [f for f in entries if os.path.isfile(os.path.join(TEMPLATES_FOLDER, f))]
     for t_file_name in template_files:
         with open(os.path.join(TEMPLATES_FOLDER, t_file_name), "r") as t_file:
-            templates.append(t_file.read())
+            templates.append({"pt_name": t_file_name, "pt_text": t_file.read()})
     return templates
+
+
+def delete_prompt_template(pt_file_name: str):
+    os.remove(os.path.join(TEMPLATES_FOLDER, pt_file_name))
+    print(f"File '{pt_file_name}' deleted successfully.")
